2c1323d6dcf1d9324140ff44908d02b1e31b37fa
Madipta <madipta@gmail.com>
2021-11-27 22:20:30 +0700

    nx 13 updates

--- libs/data-curl/jest.config.js
+++ libs/data-curl/jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   displayName: 'data-curl',
   preset: '../../jest.preset.js',
   globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-    },
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
   },
   testEnvironment: 'node',
   transform: {
--- libs/redis-cache/src/config/redis-cache.config.schema.ts
+++ libs/redis-cache/src/config/redis-cache.config.schema.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const RedisCacheConfigSchema = Joi.object({
-  REDIS_CACHE_HOST: Joi.string().default('172.28.1.4'),
+  REDIS_CACHE_HOST: Joi.string().default('localhost'),
   REDIS_CACHE_PORT: Joi.number().default(6379),
   REDIS_CACHE_TTL: Joi.number().default(3600),
   REDIS_CACHE_MAX: Joi.number().default(10000),
