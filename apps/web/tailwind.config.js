c9b821df487486b1e97266c6c7a3b3ccc34389ba
Madipta <madipta@gmail.com>
2021-12-03 20:47:19 +0700

    nx 13 update + nextjs + tailwindcss

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,19 @@
+const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
+module.exports = {
+  mode: 'jit',
+  presets: [require('../../tailwind-workspace-preset.js')],
+  purge: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
