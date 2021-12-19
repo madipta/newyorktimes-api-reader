0d65a4145fee8d3eb7386bb5c40b7e1a41943390
Madipta <madipta@gmail.com>
2021-12-19 21:40:29 +0700

    ui refactoring

--- apps/web/pages/section/[id].tsx
+++ apps/web/pages/section/[id].tsx
@@ -1,61 +1,70 @@
 import { ITopStoriesResult, TOP_STORIES_SECTIONS } from '@base/dtos';
 import axios from 'axios';
 import Image from 'next/image';
+import TopStoriesSections from '../../components/top-stories-sections/top-stories-sections';
 
 export interface SectionProps {
   id: string;
   data: ITopStoriesResult[];
+  topStoriesSections: string[];
 }
 
 export function Section(props: SectionProps) {
   const { data } = props;
   return (
-    <div className="container grid grid-cols-12 md:gap-x-14 px-4 mt-10 mx-auto">
-      {data &&
-        data.map((data) => {
-          const { abstract, byline, multimedia, title, url } = data;
-          const image = multimedia?.find((val) => val.type === 'image');
-          return (
-            <div
-              key={url}
-              className="col-span-12 md:col-span-6 grid grid-cols-12 mb-12"
-            >
-              <div className="col-span-12 md:col-span-8">
-                <a
-                  href={url}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="leading-snug font-semibold text-lg text-gray-800 cursor-pointer"
-                >
-                  {title}
-                </a>
-                <p className="text-sm text-gray-600 mt-2">{abstract}</p>
-              </div>
-              {image && (
-                <div className="col-span-12 md:col-span-4 md:pl-4 mt-4 md:mt-0">
-                  <div className="md:max-h-48 overflow-hidden">
-                    <Image
-                      src={image.url}
-                      alt={image.caption}
-                      width={image.width}
-                      height={image.height}
-                      layout="responsive"
-                      className="object-cover"
-                    ></Image>
-                  </div>
-                  <p className="leading-none font-sans text-xs text-right text-gray-400 mt-1">
-                    {byline}
-                  </p>
+    <>
+      <TopStoriesSections
+        sections={props.topStoriesSections}
+        selected={props.id}
+      ></TopStoriesSections>
+      <div className="container grid grid-cols-12 md:gap-x-14 px-4 mt-10 mx-auto">
+        {data &&
+          data.map((data) => {
+            const { abstract, byline, multimedia, title, url } = data;
+            const image = multimedia?.find((val) => val.type === 'image');
+            return (
+              <div
+                key={url}
+                className="col-span-12 md:col-span-6 grid grid-cols-12 mb-12"
+              >
+                <div className="col-span-12 md:col-span-8">
+                  <a
+                    href={url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="leading-snug font-semibold text-lg text-gray-800 cursor-pointer"
+                  >
+                    {title}
+                  </a>
+                  <p className="text-sm text-gray-600 mt-2">{abstract}</p>
                 </div>
-              )}
-            </div>
-          );
-        })}
-    </div>
+                {image && (
+                  <div className="col-span-12 md:col-span-4 md:pl-4 mt-4 md:mt-0">
+                    <div className="md:max-h-48 overflow-hidden">
+                      <Image
+                        src={image.url}
+                        alt={image.caption}
+                        width={image.width}
+                        height={image.height}
+                        layout="responsive"
+                        className="object-cover"
+                      ></Image>
+                    </div>
+                    <p className="leading-none font-sans text-xs text-right text-gray-400 mt-1">
+                      {byline}
+                    </p>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+      </div>
+    </>
   );
 }
 
 export async function getStaticProps(context) {
+  const topStoriesSections = TOP_STORIES_SECTIONS;
   const topStoriesUrl = `${process.env.API_PROTOCOL}://${process.env.API_HOST}:${process.env.API_PORT}/api/top-stories?section=${context.params.id}`;
   const response = await axios.get(topStoriesUrl, {
     timeout: 5000,
@@ -67,6 +76,7 @@ export async function getStaticProps(context) {
   return {
     props: {
       id: context.params.id,
+      topStoriesSections,
       data,
     },
     revalidate: 600,
