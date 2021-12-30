05c87e32b31934483ccb20c3183bff029650ad9c
Madipta <madipta@gmail.com>
2021-12-30 08:39:35 +0700

    tailwindcss update

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,18 +2,13 @@ const { join } = require('path');
 const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 
 module.exports = {
-  mode: 'jit',
   presets: [require('../../tailwind-workspace-preset.js')],
-  purge: [
+  content: [
     join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
     join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
